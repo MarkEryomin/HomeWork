e47ae464e1eea7cfb56d01b21633278e4949ce2a
Mark <erjomin.mark@gmail.com>
2023-03-13 14:44:27 +0300

    end task2

--- sem1/task_2.py
+++ sem1/task_2.py
@@ -4,3 +4,29 @@
 
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+flag=True
+while flag:
+    n=input('Введите трёхзначное число : ')
+    if n.isdigit() == False :
+        print('Ввели не число !!!')
+        print('Попробуйте ещё !!!')
+    else:
+        n=int(n)
+        if n<100 or n>1000:
+            print('Ввели не трёхзначное число !!!')
+            print('Попробуйте ещё !!!')
+        else:
+            flag=False
+
+sum=0
+while n>0:
+    x=n%10
+    sum=sum+x
+    n=n//10
+print(sum)
+
+
+
+
+  

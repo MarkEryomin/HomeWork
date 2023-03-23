a3784dbe6d0998353d9a620190ea100086fdea45
Mark <erjomin.mark@gmail.com>
2023-03-24 00:27:47 +0300

    end  homeWork4+etalon

--- sem_4/task_22.py
+++ sem_4/task_22.py
@@ -5,6 +5,28 @@
 # m - кол-во элементов второго множества.
 # Затем пользователь вводит сами элементы множеств
 
+# Эталон
+
+# mol = [int(x) for x in input().split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
+# a = [int(x) for x in input().split()]
+# k = set(a)
+# for i in k:
+#     set_1.add(i)
+# b = [int(x) for x in input().split()]
+# k1 = set(b)
+# for i in k1:
+#     set_2.add(i)
+# lok = set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#     print(i, end=' ')
+
 n=int(input("Введите количество элементов первого набора : "))
 m=int(input("Введите количество элементов второго набора : "))
 
@@ -41,12 +63,12 @@ else :
                 list_res.append(list2[i])
 print (list_res)
 
-# temp=0
-# for i in range(len(list_res)):
-#     for j in range(len(list_res)-1,-1):
-#         if list_res[i]>list_res[j]:
-#             temp=list_res[i]
-#             list_res[i]=list_res[j]
-#             list_res[j]=temp
+temp=0
+for i in range(len(list_res)):
+    for j in range(len(list_res)-1,-1):
+        if list_res[i]>list_res[j]:
+            temp=list_res[i]
+            list_res[i]=list_res[j]
+            list_res[j]=temp
 
-# print (list_res)
+print (list_res)

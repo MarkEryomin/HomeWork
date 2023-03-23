923e305d7162512806e78def771e28321c39c8bf
Mark <erjomin.mark@gmail.com>
2023-03-24 00:19:36 +0300

    end  homeWork4

--- sem_4/task_22.py
+++ sem_4/task_22.py
@@ -4,3 +4,49 @@
 # Пользователь вводит 2 числа. n - кол-во элементов первого множества.
 # m - кол-во элементов второго множества.
 # Затем пользователь вводит сами элементы множеств
+
+n=int(input("Введите количество элементов первого набора : "))
+m=int(input("Введите количество элементов второго набора : "))
+
+def completion(list,num):
+    for i in range(num):
+        list.append(int(input(f"Введите {i+1} элемент : ")))
+    print(list)
+    return list
+
+list1=list()
+list2=list()
+completion(list1,n)
+completion(list2,m)
+setlist1=set(list1)
+setlist2=set(list2)
+print(setlist1)
+print(setlist2)
+list_res=list()
+list1=list(setlist1)
+list2=list(setlist2)
+print(list1)
+print(list2)
+
+
+if len(list1)>len(list2):
+    for i in range(len(list1)):
+        for j in range(len(list2)):
+            if list1[i]==list2[j]:
+                list_res.append(list1[i])
+else :
+    for i in range(len(list2)):
+        for j in range(len(list1)):
+            if list2[i]==list1[j]:
+                list_res.append(list2[i])
+print (list_res)
+
+# temp=0
+# for i in range(len(list_res)):
+#     for j in range(len(list_res)-1,-1):
+#         if list_res[i]>list_res[j]:
+#             temp=list_res[i]
+#             list_res[i]=list_res[j]
+#             list_res[j]=temp
+
+# print (list_res)
--- sem_4/task_24.py
+++ sem_4/task_24.py
@@ -10,3 +10,35 @@
 # Напишите программу для нахождения максимального числа ягод,
 # которое может собрать за один заход собирающий модуль, находясь 
 # перед некоторым кустом заданной во входном файле грядки
+
+import random
+
+while(True):
+    beds_number = input("Введите количество грядок: ")
+    if(beds_number.isdigit() == False or int(beds_number) < 3):
+        print("Введите ЧИСЛО от трех")
+    else:
+        break
+
+beds_array = [random.randint(1, 100) for _ in range(int(beds_number))]
+print(beds_array)
+
+last_element = beds_array[-2]
+last_element_index = -2
+next_element = beds_array[0]
+next_element_index = 0
+temp = beds_array[-2] + beds_array[-1] + beds_array[0]
+result = temp
+result_position = -1
+for i in range(-2, len(beds_array) - 3):
+    next_element = beds_array[next_element_index + 1]
+    next_element_index += 1
+    temp += next_element - last_element
+    last_element = beds_array[last_element_index + 1]
+    last_element_index += 1
+    if result < temp:
+        result = temp
+        result_position = next_element_index
+    if result_position==-1:
+        result_position=beds_number
+print("Максимальное количество ягод: " + str(result) + "\nНужно начинать с куста под номером: " + str(result_position))

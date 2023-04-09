d1586a3c084ecde16966ec44c56052847ddfdedc
Mark <erjomin.mark@gmail.com>
2023-04-09 20:33:29 +0300

    homeWork_8

--- sem_8/task_49.py
+++ sem_8/task_49.py
@@ -27,6 +27,53 @@ def write_file(filename, data_array):
             write_element = ', '.join(i)
             data.write(f'{write_element}\n')
 
+def search_parameters():
+    print('По какому полю выполнить поиск?')
+    search_field = input('1 - по фамилии\n2 - по имени\n3 - по отчеству\n4 - по номеру телефона')
+    print()
+    search_value = None
+    if search_field == '1':
+        search_value = input('Введите фамилию для поиска: ')
+        print()
+    elif search_field == '2':
+        search_value = input('Введите имя для поиска: ')
+        print()
+    elif search_field == '3':
+        search_value = input('Введите отчество для поиска: ')
+        print()
+    elif search_field == '4':
+        search_value = input('Введите номер для поиска: ')
+        print()
+    return search_field, search_value
+
+def search_to_modify(data_array: list):
+    search_field, search_value = search_parameters()
+    search_result = []
+    for contact in data_array:
+        if contact[int(search_field)] == search_value:
+            search_result.append(search_value)
+    if len(search_result) == 1:
+        print (search_result)
+        return search_result[0]
+    elif len(search_result) > 1:
+        print('Найдено несколько контактов')
+        for i in range(len(search_result)):
+            print(f'{i + 1} - {search_result[i]}')
+        num_count = int(input('Выберите номер контакта, который нужно изменить/удалить: '))
+        return search_result[num_count - 1]
+    else:
+        print('Контакт не найден')
+    print()
+        
+def delete_contact(filename):
+    data_array = read_file(filename)
+    number_to_change = search_to_modify(data_array)
+    data_array.remove(number_to_change)
+    with open(filename, 'w', encoding='utf-8') as file:
+        for contact in data_array:
+            line = ' '.join(contact) + '\n'
+            file.write(line)
+
 def add_item(filename, lastname = '', firstname = '', secondname = '', phone = ''):
     data_array = read_file(filename) 
     max_id = 0
@@ -68,6 +115,10 @@ def menu():
         show_all_items(filename)
     elif user_operation == 2: 
         add_item(filename)
+    elif user_operation == 3: 
+        delete_contact(filename)
+
+        
 
 filename = 'file.txt'
 menu()

a54378d3e8364407a9a51515e307556a47bfd265
Mark <erjomin.mark@gmail.com>
2023-04-11 23:52:35 +0300

    end HomeWork_9

--- sem_9/task_40_42.py
+++ sem_9/task_40_42.py
@@ -4,3 +4,15 @@
 
 # Задача 42: Узнать какая максимальная households в зоне минимального значения population
 
+import pandas as pd
+df=pd.read_csv('california_housing_train.csv')
+
+df.info()
+
+df.describe()
+
+df[(df['population']>=0)&(df['population']<=500)]
+
+df[(df['population']>=0)&(df['population']<=500)]['median_house_value'].median()
+
+df[df['population']==df['population'].min()]['households'].max()
